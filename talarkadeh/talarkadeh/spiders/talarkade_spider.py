0abae0d1be2eb46087433003d7ecaeddd610d07b
JavadSarlakHOME <abtinafraz@gmail.com>
2024-12-29 11:07:36 +0330

    Add scraper

--- talarkadeh/talarkadeh/spiders/talarkade_spider.py
+++ talarkadeh/talarkadeh/spiders/talarkade_spider.py
@@ -1,19 +1,27 @@
 import scrapy
 
+class TalarkadehSpider(scrapy.Spider):
+    name = "talarkadeh"
 
-class TalarkadeSpider(scrapy.Spider):
-    name = 'example'
-    start_urls = ['https://example.com/articles']
+    def __init__(self, url, whereSave, untilPage, *args, **kwargs):
+        super(TalarkadehSpider, self).__init__(*args, **kwargs)
+        self.url = url
+        self.whereSave = whereSave
+        self.untilPage = int(untilPage)
+        self.start_urls = [url]
 
     def parse(self, response):
-        # استخراج عنوان مقاله‌ها از صفحه
-        for article in response.css('div.article'):
-            yield {
-                'title': article.css('h2.title::text').get(),
-                'url': article.css('a::attr(href)').get(),
-            }
+        links = response.css('a.card__title::attr(href)').getall()
+        full_links = [response.urljoin(link) for link in links]
 
-        # صفحه بعدی
-        next_page = response.css('a.next::attr(href)').get()
-        if next_page:
-            yield response.follow(next_page, self.parse)
+        # ذخیره کردن داده‌ها در فایل
+        with open(self.whereSave, "a", encoding="utf-8") as f:
+            for url in full_links:
+                f.write(url + "\n")
+
+        # پیدا کردن صفحه بعدی
+        current_page = int(response.url.split("/")[-1]) if "page" in response.url else 1
+        if current_page < self.untilPage:
+            next_page = f"{self.url}/page/{current_page + 1}"
+            yield scrapy.Request(next_page, callback=self.parse)
+
